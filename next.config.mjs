10d6224a4a2c6b58392f9a007c6dfab71773fbc7
Dan Skiba <skdenx@gmail.com>
2024-04-24 00:19:23 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/biometric-auth',
   reactStrictMode: true,
   output: "export",  // <=== enables static exports
   typescript: {
