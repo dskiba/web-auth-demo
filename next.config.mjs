42e5a7d201197dbe4664f055b077529a3d8603e9
Dan Skiba <skdenx@gmail.com>
2024-04-24 00:33:55 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",  // <=== enables static exports
   typescript: {
     ignoreBuildErrors: true,
   },
